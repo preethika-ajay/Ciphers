fbe1f24a2bb0b5eb91f35eb4ecb6cb0c31a4d5d5
JoyenBenitto <75515758+JoyenBenitto@users.noreply.github.com>
2021-10-16 08:08:10 +0530

    ceaser -function for encryption and decryption

---
+++ ceaser-enc-dyc-functions.cpp
@@ -0,0 +1,43 @@
+ void encrypt_ceaser(){
+ char ch;
+      for(int i = 0; msg[i] != '\0'; ++i) {
+         ch = msg[i];
+         //encrypt for lowercase letter
+         If (ch >= 'a' && ch <= 'z'){
+            ch = ch + key;
+            if (ch > 'z') {
+               ch = ch - 'z' + 'a' - 1;
+            }  
+            msg[i] = ch;
+         }
+         //encrypt for uppercase letter
+         else if (ch >= 'A' && ch <= 'Z'){
+            ch = ch + key;
+            if (ch > 'Z'){
+               ch = ch - 'Z' + 'A' - 1;
+            }
+            msg[i] = ch;}
+      }}
+
+void decrypt_ceaser()
+{
+    char ch;
+      for(int i = 0; msg[i] != '\0'; ++i) {
+         ch = msg[i];
+         //decrypt for lowercase letter
+         if(ch >= 'a' && ch <= 'z') {
+            ch = ch - key;
+            if(ch < 'a'){
+               ch = ch + 'z' - 'a' + 1;
+            }
+            msg[i] = ch;
+         }
+         //decrypt for uppercase letter
+         else if(ch >= 'A' && ch <= 'Z') {
+            ch = ch - key;
+            if(ch < 'A') {
+               ch = ch + 'Z' - 'A' + 1;
+            }
+            msg[i] = ch;
+         }
+}}
